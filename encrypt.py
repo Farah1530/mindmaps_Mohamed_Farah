6a1e89ffbca45b367dfcf41861bf2031943dc14d
Farah Mohamed Ahmed <ps51mdf@eduvaud.ch>
2026-05-04 15:30:02 +0200

    corrigé des bug

--- encrypt.py
+++ encrypt.py
@@ -1,3 +1,8 @@
+# cette partie gère l'affichage de la fenêtre d'inscription
+# Farah MOHAMED pour SI-CA1 (projet Python) - 2025-2026
+# 29 avril 2026
+# encrypt.py : affichage de la fenêtre d'inscription
+
 
 import bcrypt
 
--- logout.py
+++ logout.py
@@ -1,3 +1,8 @@
+# cette partie gère l'affichage de la fenêtre d'inscription
+# Farah MOHAMED pour SI-CA1 (projet Python) - 2025-2026
+# 29 avril 2026
+# logout.py : affichage de la fenêtre de déconnexion
+
 import utils.session
 
 def logout():
